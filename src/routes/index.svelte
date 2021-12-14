1024755662d01765f4dc2f25820f35ab498958a2
Brent Vanwildemeersch <brent.vanwildemeersch@easyfairs.com>
2021-12-14 17:51:28 +0100

    Remove bootstrapped unnecessary files

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,24 +10,11 @@
 	<title>Home</title>
 </svelte:head>
 
-<section>
+<main>
 	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
-
-		to your new<br />SvelteKit app
+		Art Institute of Chicago
 	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
+</main>
 
 <style>
 	section {
